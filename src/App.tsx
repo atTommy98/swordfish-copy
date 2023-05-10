86573046bc0583013d9d9e59055407d8b746e9ce
Tommy Holt <tommy_holt@icloud.com>
2023-05-10 15:13:20 +0100

    360 video can now be manipulated and works on mobile iOS and Android browsers

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import "./index.css";
 import url from "./videos/mountain-pass.mp4";
+import {useState} from "react"
 
 // Components
 import ThreeCanvas from "./components/ThreeCanvas";
 import Accelerometer from "./components/Accelerometer";
 
 function App() {
+  const [video] = useState(() => {
+    const vid = document.createElement("video");
+    vid.setAttribute('playsinline', "true")
+    vid.src = url;
+    vid.crossOrigin = "Anonymous";
+    vid.loop = true;
+    vid.muted = true;
+    vid.play();
+    return vid;
+  });
+
+  // video.play()
 
   return (
     <div className="App">
-      <ThreeCanvas url={url} />
+      <ThreeCanvas url={video} />
       <Accelerometer />
     </div>
   );
--- src/components/VideoSphere.tsx
+++ src/components/VideoSphere.tsx
@@ -1,22 +1,11 @@
-import video from "../videos/mountain-pass.mp4";
-import { useVideoTexture } from "@react-three/drei";
-import THREE from "three";
-
 export default function VideoSphere({ url }: any) {
-  // const videoTexture = useVideoTexture(video)
-  // videoTexture.source.data.paused = true
-  // console.log(videoTexture.source.data)
-  // const texture = useVideoTexture(url)
-
-  const texture = useVideoTexture(url)
-
-  console.log(texture.source.data)
-
 
   return (
     <mesh>
       <sphereGeometry attach="geometry" args={[2, 64, 64]} />
-      <meshStandardMaterial attach="material" side={2} map={texture} />
+      <meshStandardMaterial attach="material" side={2}>
+        <videoTexture attach="map" args={[url]} />
+      </meshStandardMaterial>
     </mesh>
   );
 }
